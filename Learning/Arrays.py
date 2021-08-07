ded75cc8eeaa60c6b3d15f578c065ab0546b89a7
Rajatbais1310 <88583582+Rajatbais1310@users.noreply.github.com>
2021-08-08 01:42:53 +0530

    Add files via upload

---
+++ Learning/Arrays.py
@@ -0,0 +1,14 @@
+myarr=[30,10,20,50,40]
+myarr1=[70,90,80,60,100]
+myarr3=[]
+# print(myarr[3])
+myarr.append("Rajat")
+myarr.pop(5)
+myarr.remove(50)
+myarr.extend(myarr1)
+myarr.sort()
+myarr.copy()
+print(myarr)
+
+# for x in myarr:
+#     print(x)
